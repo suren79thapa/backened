cbebbbceeff364c48e2946e1a35783a58e839b08
suren79thapa <thapasurendra447@gmail.com>
2025-09-05 10:34:01 +0545

    added change for route

--- index.js
+++ index.js
@@ -22,6 +22,8 @@ app.use(
   cors({
     origin: allowedOrigins,
     credentials: true,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
